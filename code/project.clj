ec185b34045355e15b201357cd49ed52ee1384bf
Hugo Duncan <hugo@hugoduncan.org>
2012-11-16 23:26:50 -0500

    Updates

--- code/project.clj
+++ code/project.clj
@@ -3,4 +3,6 @@
   :url "https://github.com/hugoduncan"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/tools.macro "0.1.1"]
+                 ])
--- code/src/ritz_conj/example.clj
+++ code/src/ritz_conj/example.clj
@@ -1,14 +1,33 @@
 (ns ritz-conj.example)
 
 ;;; simple exception
-
-(comment (/ 1 0))
+(comment
+  (/ 1 0))
 
 (defn f1 []
   (let [a 1]
     (/ a 0)))
 
 (comment (f1))
+
+(defn f5 []
+  (let [c (range 10)]
+    (/ (first c) 0)))
+
+(comment (f5))
+
+
+;;; Tracking down errors
+(comment
+  (defmulti fred [x] x))
+
+(comment
+  (ns fred
+    (:use
+     [clojure.string :only join])))
+
+
+
 
 ;; (defn f2 []
 ;;   (let [a (map inc (range 1000))]
@@ -31,19 +50,3 @@
 ;;     (f1)))
 
 ;; (comment (f4))
-
-(defn f5 []
-  (let [c (range 10)]
-    (/ (first c) 0)))
-
-(comment (f5))
-
-
-;;; Tracking down errors
-(comment
-  (defmulti fred [x] x))
-
-(comment
-  (ns fred
-    (:use
-     [clojure.string :only join])))
